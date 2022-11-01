4359774de0e5e133150f44dbbe5826cad53d837d
Azdevpp <azdevpp@gmail.com>
2022-11-01 17:16:32 +0200

    SincronizePasswordValidatore

---
+++ src/app/auth/Validators/match-password.ts
@@ -0,0 +1,19 @@
+import { Injectable } from '@angular/core';
+import {
+  AbstractControl,
+  FormGroup,
+  ValidationErrors,
+  Validator,
+} from '@angular/forms';
+
+@Injectable({ providedIn: 'root' })
+export class MatchPassword implements Validator {
+  validate(formGroup: AbstractControl) {
+    const { password, passwordConfirmation } = formGroup.value;
+    if (password === passwordConfirmation) {
+      return null;
+    } else {
+      return { passwordsDontMatch: true };
+    }
+  }
+}
