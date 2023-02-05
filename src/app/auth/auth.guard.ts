ab74362850fe8f71748bcf07a42e92f9aecdec0a
Azdevpp <Azdevpp@gmail.com>
2023-02-05 23:23:36 +0200

    Implementing the auth Guard canLoad function

--- src/app/auth/auth.guard.ts
+++ src/app/auth/auth.guard.ts
@@ -1,14 +1,29 @@
 import { Injectable } from '@angular/core';
 import { CanLoad, Route, UrlSegment, UrlTree } from '@angular/router';
-import { Observable } from 'rxjs';
+import { Observable, map, skipWhile, take } from 'rxjs';
+import { AuthService } from './auth.service';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class AuthGuard implements CanLoad {
+  constructor(private authService: AuthService) {}
   canLoad(
     route: Route,
-    segments: UrlSegment[]): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
-    return true;
+    segments: UrlSegment[]
+  ):
+    | Observable<boolean | UrlTree>
+    | Promise<boolean | UrlTree>
+    | boolean
+    | UrlTree {
+    // return new Observable((subscriber) => {
+    // subscriber.next(false);
+    // subscriber.complete();
+    return this.authService.signedin$.pipe(
+      skipWhile((value) => value === null),
+      map((value) => value!),
+      take(1)
+    );
+    // });
   }
 }
