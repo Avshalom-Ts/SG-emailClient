b810abdfc8b65781c18996fba27b12faf8265c69
Azdevpp <Azdevpp@gmail.com>
2023-02-06 23:10:13 +0200

    Some Code Cleaning

--- src/app/inbox/email-show/email-show.component.ts
+++ src/app/inbox/email-show/email-show.component.ts
@@ -1,7 +1,5 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
-import { EmailService } from '../email.service';
-import { switchMap } from 'rxjs';
 import { Email } from '../email';
 
 @Component({
@@ -11,42 +9,10 @@ import { Email } from '../email';
 })
 export class EmailShowComponent implements OnInit {
   email: Email | undefined;
-  constructor(
-    private route: ActivatedRoute,
-    private emailService: EmailService
-  ) {
-    // console.log(this.route.snapshot.data);
+  constructor(private route: ActivatedRoute) {
     this.route.data.subscribe(({ email }) => {
       this.email = email;
     });
   }
-
-  // ! way 1 with nested subscribe
-  //? Its bad aproutch becose when new route will invoc by the user
-  //? and the curent invoc will not reach the target it will coss a delay betwin the requests
-  // ngOnInit(): void {
-  //   // console.log(this.route);
-  //   this.route.params.subscribe(({ id }) => {
-  //     // console.log(id);
-  //     this.emailService.getEmail(id).subscribe((email) => {
-  //       console.log(email);
-  //     });
-  //   });
-  // }
-
-  // ! way 2 with rxjs
-  // ? if there will be anotheres invocs from the user the previus will canceld
-  // ? and the amswer will be from the last invocet.
-  ngOnInit(): void {
-    // this.route.params
-    //   .pipe(
-    //     switchMap(({ id }) => {
-    //       return this.emailService.getEmail(id);
-    //     })
-    //   )
-    //   .subscribe((email) => {
-    //     // console.log(email);
-    //     this.email = email;
-    //   });
-  }
+  ngOnInit(): void {}
 }
