81c130b72b7a529d12cc8293c3339670ebd3fbcd
Azdevpp <Azdevpp@gmail.com>
2023-02-06 20:09:43 +0200

    Implemented the Get email method and render it in the inbox component inside home && Generated the Placeholder component

--- src/app/inbox/email.service.ts
+++ src/app/inbox/email.service.ts
@@ -1,9 +1,20 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 
+interface EmailSummary {
+  id: string;
+  subject: string;
+  from: string;
+}
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class EmailService {
+  rootUrl = 'https://api.angular-email.com';
+  constructor(private http: HttpClient) {}
 
-  constructor() { }
+  getEmails() {
+    //! Dont need to put {withCredentials:true} becouse we use the interceptore globaly for the app
+    return this.http.get<EmailSummary[]>(this.rootUrl + '/emails');
+  }
 }
