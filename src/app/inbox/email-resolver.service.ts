95dc436281ac2c21fc3fd0edb5d2f5d65ae85604
Azdevpp <Azdevpp@gmail.com>
2023-02-07 18:23:44 +0200

    Implementing the Not-found email page

--- src/app/inbox/email-resolver.service.ts
+++ src/app/inbox/email-resolver.service.ts
@@ -3,17 +3,25 @@ import {
   ActivatedRouteSnapshot,
   Resolve,
   RouterStateSnapshot,
+  Router,
 } from '@angular/router';
 import { Email } from './email';
+import { EMPTY } from 'rxjs';
 import { EmailService } from './email.service';
+import { catchError } from 'rxjs';
 
 @Injectable({
   providedIn: 'root',
 })
 export class EmailResolverService implements Resolve<Email> {
-  constructor(private emailService: EmailService) {}
+  constructor(private emailService: EmailService, private router: Router) {}
   resolve(route: ActivatedRouteSnapshot, state: RouterStateSnapshot) {
     const { id } = route.params;
-    return this.emailService.getEmail(id);
+    return this.emailService.getEmail(id).pipe(
+      catchError(() => {
+        this.router.navigateByUrl('/inbox/not-found');
+        return EMPTY;
+      })
+    );
   }
 }
